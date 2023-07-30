3c373f4d80965f17c289b171f3fa470608a24b44
nobel7761 <habiburnobel@gmail.com>
2023-07-31 01:08:27 +0600

    usePostCommentMutations implemented using RTK Query

--- src/redux/api/apiSlice.ts
+++ src/redux/api/apiSlice.ts
@@ -10,7 +10,19 @@ export const api = createApi({
     getSingleProducts: builder.query({
       query: (id) => `/product/${id}`,
     }),
+    postComment: builder.mutation({
+      //if we want to send multiple argument, we have to use object.
+      query: ({ id, data }) => ({
+        url: `/comment/${id}`,
+        method: 'POST',
+        body: data,
+      }),
+    }),
   }),
 });
 
-export const { useGetProductsQuery, useGetSingleProductsQuery } = api;
+export const {
+  useGetProductsQuery,
+  useGetSingleProductsQuery,
+  usePostCommentMutation,
+} = api;
